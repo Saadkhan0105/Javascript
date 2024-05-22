3d40937dec87a8d7578bdda8372d2672dd4ba35e
Saad Khan <134200427+Saadkhan0105@users.noreply.github.com>
2024-05-22 10:31:00 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Saad")
+
+
+let name = "Saad"
+let age = 28
+let isLoggedIn = false
+let state;
+// number => 2 to power 53
+// bigint
+// string => true/false
+// null => standalone value
+// undefined =>
+// symbol => to find uniqueness 
+
+
+// object - example is given below
+
+console.log(typeof undefined); // output => undefined
+console.log(typeof null); // output => object
