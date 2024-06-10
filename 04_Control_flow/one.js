a06b98ab4ad1cf433ccb16e2e8e237d4090aa713
Saad Khan <134200427+Saadkhan0105@users.noreply.github.com>
2024-06-10 16:57:37 +0000

    Control flow

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,48 @@
+// if
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+
+// console.log("Executed");
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "Fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+
+// if (balance > 500) console.log("Test"), console.log("Test2");
+
+// if (balance < 500) {
+//     console.log("Less than 500");
+// } else if (balance < 750) {
+//     console.log("Less than 750");
+// } else if (balance < 900) {
+//     console.log("Less than 900");
+// } else {
+//     console.log("Balance is Less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
