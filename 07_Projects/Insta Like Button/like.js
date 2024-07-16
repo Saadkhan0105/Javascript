b3a45641703f89d5ab01cdb415a8d5ca90201825
Saad Khan <134200427+Saadkhan0105@users.noreply.github.com>
2024-07-16 13:46:12 +0000

    Instagram Like Button

---
+++ 07_Projects/Insta Like Button/like.js
@@ -0,0 +1,14 @@
+const cont = document.querySelector("#container");
+const love = document.querySelector("i");
+cont.addEventListener("dblclick", function(){
+    love.style.transform = "translate(-50%, -50%) scale(1)"
+    love.style.opacity = 0.8
+
+    setTimeout(function(){
+        love.style.opacity = 0
+    }, 1000)
+    setTimeout(function(){
+        love.style.transform = "translate(-50%, -50%) scale(1)"
+    }, 2000)
+})
+
